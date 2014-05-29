a52e6c4bd27c08a4a2006c2719fa6c37ef6135fe
mandrews <mandrews@memsql.com>
2014-05-28 17:32:14 -0700

    Reduced dependency on clientserver and consequently X

--- exterminator.vim
+++ exterminator.vim
@@ -1,21 +1,23 @@
 
-function! StartDebugger(...)
-    Rooter
-    if len(v:servername) == 0
-        echo "Debugging only works with vim started in server mode!"
-    endif
-    exec 'silent ! tmux split -p 30 "VIM_DEBUG_SERVER=' . v:servername . ' ' . g:exterminator_dir . '/dbg '. join(a:000, ' ') . '"'
-endfunction
-
 python << EOF
 
 import vim
-import sys, os, psutil, subprocess, threading
+import sys, os, psutil, subprocess, threading, json, time
 sys.path.append(os.path.expanduser(vim.eval("g:exterminator_dir")))
 import gdb_fancy
 
-def InitRemoteGdb():
-    vim.gdb = gdb_fancy.RemoteGdb(vim)
+def InitRemoteGdbWithFile(exterminator_file):
+    while True:
+       try:
+           data = open(exterminator_file, 'r').read()
+           host, port = json.loads(open(exterminator_file, 'r').read())
+           break
+       except ValueError, IOError:
+           time.sleep(0.1)
+    InitRemoteGdb(host, port)
+
+def InitRemoteGdb(host, port):
+    vim.gdb = gdb_fancy.RemoteGdb(vim, host, port)
 
 def get_loc():
     return vim.current.buffer.name, vim.current.range.start + 1
@@ -41,13 +43,35 @@ def display(data, window_name='mandrews', new_command='bot 15new'):
     finally:
         vim.command("%swincmd w" % winnr)
 
+def HandleEvents():
+    try:
+        vim.gdb.handle_events()
+    except:
+        pass
+
 EOF
 
-comm! -nargs=0 GdbToggle   python vim.gdb.toggle_break(*get_loc())
-comm! -nargs=0 GdbContinue python vim.gdb.send_continue()
-comm! -nargs=0 GdbNext     python vim.gdb.send_next()
-comm! -nargs=0 GdbStep     python vim.gdb.send_step()
-comm! -nargs=0 GdbEval     python display("%s:\n%s" % (get_tok(), vim.gdb.eval_expr(get_tok())))
+function! StartDebugger(...)
+    Rooter
+    let g:exterminator_file = substitute(system('mktemp'), '\n$', '', '')
+    exec 'silent ! tmux split -p 30 -h "VIM_SERVER=' . v:servername . ' EXTERMINATOR_FILE=' . g:exterminator_file . ' ' . g:exterminator_dir . '/dbg '. join(a:000, ' ') . '"'
+    if len(v:servername) == 0
+        python InitRemoteGdbFromFile(vim.eval("g:exterminator_file"))
+    endif
+    unlet g:exterminator_file
+    wincmd =
+endfunction
+
+if exists('g:exterminator_file')
+    python InitRemoteGdb(vim.eval("g:exterminator_file"))
+    unlet g:exterminator_file
+endif
+
+comm! -nargs=0 GdbToggle    python vim.gdb.toggle_break(*get_loc())
+comm! -nargs=0 GdbContinue  python vim.gdb.send_continue()
+comm! -nargs=0 GdbNext      python vim.gdb.send_next()
+comm! -nargs=0 GdbStep      python vim.gdb.send_step()
+comm! -nargs=0 GdbEval      python vim.gdb.eval_expr(get_tok())
 comm! -nargs=+ Dbg         call StartDebugger(<f-args>)
 
 highlight SignColumn guibg=Black guifg=White ctermbg=None ctermfg=White
@@ -56,3 +80,4 @@ sign define breakpoint text=>> texthl=Comment
 sign define just_pc text=-- texthl=Debug
 sign define pc_and_breakpoint text=-> texthl=Debug
 
+au CursorHold *             python HandleEvents()
--- gdb_fancy.py
+++ gdb_fancy.py
@@ -1,10 +1,13 @@
-import os, signal, select
+import os, signal, select, json
 from multiprocessing import Process, Pipe
 from multiprocessing.connection import Listener, Client
 
 class Gdb(object):
     def __init__(self, sock, proxy):
-        self.vimserver = str(gdb.parse_and_eval("$vim"))
+        try:
+            self.servername = os.environ['VIM_SERVER']
+        except KeyError:
+            self.servername = None
         self.sock = sock
         self.next_breakpoint = 2
         self.proxy = proxy
@@ -17,29 +20,28 @@ class Gdb(object):
         assert hello['op'] == 'init', str(hello)
 
         self.port = hello['port']
-
-        self.vim('let g:GDB_PORT=%d' % self.port)
-        self.vim('python InitRemoteGdb()')
+        if self.servername:
+            os.system('vim --servername %s --remote-send "<esc><esc>:python InitRemoteGdb(\'localhost\', %d)<cr>"' % (self.servername, self.port))
 
     def attach_hooks(self):
         def on_prompt(prompt):
             self.handle_events()
             try:
                 self.goto_frame(gdb.selected_frame())
-                self.mark_breakpoints()
             except gdb.error:
                 pass
-            except:
-                raise
+            self.mark_breakpoints()
         gdb.prompt_hook = on_prompt
 
         def kill_server(e):
-            self.vim('sign unplace *')
-            self.proxy.terminate()
+            self.vim(op='quit')
+            self.vim(dest='proxy', op='quit')
         gdb.events.exited.connect(kill_server)
 
-    def vim(self, cmd):
-        os.system(("vim --servername " + self.vimserver + " --remote-send '<esc><esc>:" + cmd + "<cr>'"))
+    def vim(self, **kwargs):
+        self.sock.send(dict({'dest': 'vim'}, **kwargs))
+        if self.servername:
+            os.system('vim --servername %s --remote-send "<esc><esc>:python HandleEvents()<cr>"' % self.servername)
 
     def handle_events(self):
         if not self.sock.poll():
@@ -53,19 +55,18 @@ class Gdb(object):
                     print str(e)
             elif c['op'] == 'eval':
                 try:
-                    c['response'] = gdb.execute(c['comm'], to_string=True)
+                    contents=gdb.execute(c['expr'], to_string=True)
                 except gdb.error as e:
-                    c['response'] = None
-                    print str(e)
-                c['dest'] = 'vim'
-                self.sock.send(c)
+                    contents = str(e)
+                print contents
+                self.vim(op='disp', expr=c['expr'], contents=contents)
             elif c['op'] == 'disable':
                 self.disable_breakpoints(*c['loc'])
             elif c['op'] == 'toggle':
                 self.toggle_breakpoints(*c['loc'])
             elif c['op'] == 'goto':
                 self.toggle_breakpoints(*c['loc'])
-            elif c['op'] == 'goto':
+            elif c['op'] == 'quit':
                 gdb.execute('quit')
             if not self.sock.poll():
                 return
@@ -73,7 +74,6 @@ class Gdb(object):
     def goto_frame(self, frame):
         filename, line = self.to_loc(frame.find_sal())
         self.goto_file(filename, line)
-        self.vim("%(line)skP" % locals())
 
     def goto_file(self, filename, line):
         if not os.path.exists(filename):
@@ -82,8 +82,7 @@ class Gdb(object):
             return
         self.filename = filename
         self.line = line
-        servername = gdb.parse_and_eval("$vim")
-        os.system("vim --servername %(servername)s --remote +%(line)s %(filename)s" % locals())
+        self.vim(op='goto', line=line, filename=filename)
 
     def to_loc(self, sal):
         return sal.symtab.filename, int(sal.line)
@@ -95,7 +94,7 @@ class Gdb(object):
 
     def mark_breakpoints(self):
         if gdb.breakpoints() is None:
-            self.vim("sign unplace *")
+            self.vim(op='unplace', num='*')
             return
         new_breakpoints = set([])
         for breakpoint in gdb.breakpoints():
@@ -108,20 +107,18 @@ class Gdb(object):
         add = new_breakpoints - old_breakpoints
 
         for filename, line in remove:
-            self.vim("sign unplace %d" % self.breakpoints[(filename, line)])
+            self.vim(op='unplace', num=self.breakpoints[(filename, line)])
             del self.breakpoints[(filename, line)]
 
         for filename, line in add:
             print filename, line
             self.breakpoints[(filename, line)] = self.next_breakpoint
-            self.vim("badd %(filename)s" % locals())
-            self.vim("sign place %d name=breakpoint line=%s file=%s" % (self.next_breakpoint, line, filename))
+            self.vim(op='place', num=self.next_breakpoint, name='breakpoint', line=line, filename=filename)
             self.next_breakpoint += 1
 
         sign = 'pc_and_breakpoint' if (self.filename, self.line) in self.breakpoints.keys() else 'just_pc'
-        self.vim("badd %s" % self.filename)
-        self.vim("sign unplace 1")
-        self.vim("sign place 1 name=%s line=%s file=%s" % (sign, self.line, self.filename))
+        self.vim(op='unplace', num=1)
+        self.vim(op='place', num=1, name=sign, line=self.line, filename=self.filename)
 
     def disable_breakpoints(self, filename, line):
         for breakpoint in gdb.breakpoints():
@@ -144,15 +141,51 @@ class Gdb(object):
         pass # Unimplemented
 
 class RemoteGdb(object):
-    def __init__(self, vim):
+    def __init__(self, vim, host, port):
         self.vim = vim
-        self.sock = Client(("localhost", int(vim.eval('g:GDB_PORT'))))
+        self.sock = Client((host, port))
 
     def send_command(self, **kwargs):
         self.sock.send(dict({'dest': 'gdb'}, **kwargs))
 
-    def receive_response(self):
-        return self.sock.recv()['response']
+    def handle_events(self):
+        if not self.sock.poll():
+            return
+        while True:
+            try:
+                c = self.sock.recv()
+            except EOFError:
+                self.vim.gdb = None
+                return
+            if c['op'] == 'goto':
+                window = self.find_window('navigation')
+                if window is None:
+                    self.claim_window('navigation')
+                self.vim.command("e +%(line)s %(filename)s" % c)
+                self.vim.command("let &ft=&ft")
+                self.vim.command("%(line)skP" % c)
+            elif c['op'] == 'disp':
+                winnr = int(self.vim.eval("winnr()"))
+                window = self.find_window('display', 'bot 15new')
+                self.vim.command("setlocal buftype=nowrite bufhidden=wipe modifiable nobuflisted noswapfile nowrap nonumber")
+                contents = [ c['expr'], c['contents'] ]
+                self.vim.current.window.buffer[:] = contents
+                self.vim.command("setlocal nomodifiable")
+                self.vim.command("%swincmd w" % winnr)
+            elif c['op'] == 'place':
+                self.vim.command("badd %(filename)s" % c)
+                self.vim.command("sign place %(num)s name=%(name)s line=%(line)s file=%(filename)s" % c)
+            elif c['op'] == 'unplace':
+                self.vim.command("sign unplace %(num)s" % c)
+            elif c['op'] == 'quit':
+                self.vim.command("sign unplace *")
+                winnr = int(self.vim.eval("winnr()"))
+                window = self.find_window('display')
+                if window is not None:
+                    self.vim.command("q")
+                self.vim.command("%swincmd w" % winnr)
+            if not self.sock.poll():
+                return
 
     def send_trap(self):
         self.send_command(dest='proxy', op='trap')
@@ -163,39 +196,66 @@ class RemoteGdb(object):
     def send_continue(self):
         self.send_command(op='exec', comm='continue')
         self.send_trap()
+        self.handle_events()
 
     def send_next(self):
         self.send_command(op='exec', comm='next')
         self.send_trap()
+        self.handle_events()
 
     def send_step(self):
         self.send_command(op='exec', comm='step')
         self.send_trap()
+        self.handle_events()
 
     def send_break(self, filename, line):
         self.send_command(op='exec', comm='break %s:%d' % (filename, line))
         self.send_trap()
+        self.handle_events()
 
     def disable_break(self, filename, line):
         self.send_command(op='disable', loc=(filename, line))
         self.send_trap()
+        self.handle_events()
 
     def toggle_break(self, filename, line):
         self.send_command(op='toggle', loc=(filename, line))
         self.send_trap()
+        self.handle_events()
 
     def continue_until(self, filename, line):
         self.send_command(op='goto', loc=(filename, line))
         self.send_trap()
+        self.handle_events()
 
     def eval_expr(self, expr):
-        self.send_command(op='eval', comm='print ' + str(expr))
+        self.send_command(op='eval', expr='print ' + str(expr))
         self.send_trap()
-        return self.receive_response()
+        self.handle_events()
+
+    def claim_window(self, window_name):
+        self.vim.command('let b:mandrews_output_window = "%s"' % window_name)
+
+    def find_window(self, window_name, new_command=None):
+        winnr = int(self.vim.eval("winnr()"))
+        while True:
+            if int(self.vim.eval("exists('b:mandrews_output_window')")) > 0:
+                if str(self.vim.eval("b:mandrews_output_window")) == window_name:
+                    break
+            self.vim.command("wincmd w")
+            if winnr == int(self.vim.eval("winnr()")):
+                if new_command is not None:
+                    self.vim.command(new_command)
+                    self.claim_window(window_name)
+                    break
+                return
+        return self.vim.current.window
 
 def HandleProxyRequest(c):
     if c['op'] == 'trap':
         os.kill(os.getppid(), signal.SIGINT)
+    if c['op'] == 'quit':
+        exit(0)
     else:
         print "Proxy packet with unknown op: " + str(c)
 
@@ -219,9 +279,10 @@ def ProxyConnection(vim_conn, gdb_conn):
         traceback.print_exc()
         pass
 
-def ProxyServer(gdb_conn):
+def ProxyServer(gdb_conn, address_file):
     try:
         server = Listener(('localhost', 0))
+        open(address_file, 'w').write(json.dumps(server.address))
         gdb_conn.send({'op': 'init', 'port': server.address[1]})
         signal.signal(signal.SIGINT, signal.SIG_IGN)
         while True:
@@ -236,8 +297,9 @@ def ProxyServer(gdb_conn):
 if __name__ == '__main__':
     import gdb
     gdb_sock, gdb_proxy = Pipe(True)
+    exterminator_file = os.environ['EXTERMINATOR_FILE']
 
-    proxy = Process(target=ProxyServer, args=(gdb_proxy,))
+    proxy = Process(target=ProxyServer, args=(gdb_proxy, exterminator_file))
     proxy.start()
 
     gdb_manager = Gdb(gdb_sock, proxy)
